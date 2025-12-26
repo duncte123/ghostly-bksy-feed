54365f8f9669a4ea9dbe13f4d0698df08b75fb09
duncte123 <contact@duncte123.me>
2025-12-26 21:47:49 +0100

    More blocked users

--- src/terms/denylist.ts
+++ src/terms/denylist.ts
@@ -54,6 +54,8 @@ const bannedText: string[] = [
   ' scrotum ',
   ' slut ',
   ' smegma ',
+  ' spank ',
+  ' spankink ',
   ' spunk ',
   ' tosser ',
   ' turd ',
@@ -61,15 +63,19 @@ const bannedText: string[] = [
   ' vagina ',
   ' wank ',
   ' whore ',
+  // *sigh*
   '#yaoi',
   '#r34',
   '#rule43',
+  '#spank',
 ]
 
 // Exclude posts from these users
 const bannedUsers: string[] = [
   'did:plc:ju7elc27naswr6mnvpevfqxn', // sperezart.bsky.social (nsfw artist, sorry, trying to keep the feed clean)
   'did:plc:rypydv2x5sswveg6haa4vbrc', // evercras.bsky.social (nsfw artist, sorry, trying to keep the feed clean)
+  'did:plc:myv7p4kiyipudlh2y4mzo5td', // otulissa.bsky.social (nsfw artist, sorry, trying to keep the feed clean)
+  'did:plc:vgvx54edh36hak6deqpz3xt5', // jac26qq308.bsky.social (nsfw artist, sorry, trying to keep the feed clean)
   'did:plc:p6cxe24t5uywfs5rzozkkomq', // zeusai.bsky.social (AI Slop)
 ]
 
